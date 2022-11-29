56d016c1c1cde6632f98bee37b5a6726c2661790
Krishnakalani111 <88764668+Krishnakalani111@users.noreply.github.com>
2022-11-29 08:12:43 +0530

    Time: 1 ms (93.53%), Space: 41.8 MB (67.95%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -11,7 +11,8 @@ class Solution {
         //The indexOf() method returns the position of the first occurrence of                  specified character(s) in a string.
         for(int i=1;i<strs.length;i++){
            while(strs[i].indexOf(prefix)!=0){
-               // taking the while till zero to find the prefix 
+               // taking the while till zero to find the prefix and as its a word we                are comparing to other word , we are reducing the size to get the                    index.
+                 
                prefix = prefix.substring(0,prefix.length()-1);
            }
         }
